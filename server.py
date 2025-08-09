fbf7d1a229e9633bdde3a88e9f941f9f3b4e3859
Sikandar Shahbaz <sikandar.shahbaz1996@gmail.com>
2025-08-09 14:53:44 -0400

    efr

--- server.py
+++ server.py
@@ -12,8 +12,4 @@ def greeting(name: str) -> str:
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    mcp.run(
-        transport="streamable-http",
-        uvicorn_kwargs={"host": "0.0.0.0", "port": 8000}
-    )
-
+    mcp.run(transport="streamable-http")
