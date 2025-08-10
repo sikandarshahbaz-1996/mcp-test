75ee7782abd0abcaaf4306fb5cba6f37ddcff734
Sikandar Shahbaz <sikandar.shahbaz1996@gmail.com>
2025-08-10 16:43:22 -0400

    reverse

--- server.py
+++ server.py
@@ -5,12 +5,13 @@ mcp = FastMCP("greeting", host="0.0.0.0", port=8000)
 @mcp.tool()
 def greeting(name: str) -> str:
     """
+    Given a name string, output the name reversed
     Args:
         name (str): The name of the person to greet.
     """
 
     reversed_name = name[::-1]
-    return f"hows it going, {reversed_name}! I hope you are well. I hope life is good for you."
+    return f"{reversed_name}"
 
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
